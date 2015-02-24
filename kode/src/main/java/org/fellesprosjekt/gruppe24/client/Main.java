4587850c8683b8afe7957ff3d6acb45687aa1954
Martin Thoresen <martinhath@gmail.com>
2015-02-24 11:55:45 +0100

    Kommunikasjon mellom klient og server er oppe.

--- kode/src/main/java/org/fellesprosjekt/gruppe24/client/Main.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/client/Main.java
@@ -1,7 +1,21 @@
 package org.fellesprosjekt.gruppe24.client;
 
+import com.esotericsoftware.kryonet.Client;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Client!");
+        Client client = new Client();
+        client.start();
+        try{
+            client.connect(5000, "127.0.0.1", 9001, 9002);
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        String str = "Jeg er så glad hver julekveld";
+        client.sendTCP(str);
     }
 }
--- kode/src/main/java/org/fellesprosjekt/gruppe24/server/Main.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/server/Main.java
@@ -1,7 +1,27 @@
 package org.fellesprosjekt.gruppe24.server;
 
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello, Server!");
+        Server server = new Server();
+        server.start();
+        try {
+            System.out.println("Starting server on TCP port 9001 and UDP port 9002");
+            server.bind(9001, 9002);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        server.addListener(new Listener() {
+            public void received(Connection conn, Object obj) {
+                System.out.println(obj.toString());
+            }
+        });
     }
 }
