035a1311b86105f30fe82fcff169ef900a45e4af
viktorfa <vikfand@gmail.com>
2015-02-27 13:36:09 +0100

    Flytta databasetester til egen pakke

---
+++ kode/src/test/java/org/fellesprosjekt/gruppe24/database/MeetingDatabaseHandlerTest.java
@@ -0,0 +1,16 @@
+package org.fellesprosjekt.gruppe24.database;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by viktor on 27.02.15.
+ */
+
+public class MeetingDatabaseHandlerTest extends TestCase {
+    public void setUp() {
+
+    }
+
+
+
+}
