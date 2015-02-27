2974f96a557c47097cf9ad83914a13a489df9039
hermanmk <lex5050@live.no>
2015-02-27 12:24:38 +0100

    Lagde UserDatabaseHandler.java

---
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/database/UserDatabaseHandler.java
@@ -0,0 +1,45 @@
+package org.fellesprosjekt.gruppe24.database;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.fellesprosjekt.gruppe24.common.models.User;
+
+public class UserDatabaseHandler {
+
+	public static ArrayList<User> getAllUsers() {
+		ArrayList<User> allUsers = new ArrayList<User>();
+		String query = "SELECT * FROM User;";
+		ArrayList<HashMap<String, String>> result = DatabaseManager.getList(query);
+		for(HashMap<String, String> hm : result) {
+			allUsers.add(generateUser(hm));
+		}
+		return allUsers;
+	}
+	
+	private static User generateUser(HashMap<String, String> info) {
+		//User user = new user(info.get("userid"), info.get("username"), info.get("email"), info.get("password"),
+		//		info.get("create_time"), info.get("update_time"));
+		// TODO: Add proper constructor so the previous lines can be uncommented.
+		User user = new User(info.get("username"));
+		return user;
+	}
+	
+	public static void addNewUser(User user) {
+		//String query = String.format("INSERT INTO User (username, email, password, create_time) VALUES"
+		//		+ " ('%s', '%s', '%s', CURRENT_TIMESTAMP)", user.getUsername());
+		// TODO: 
+		String query = String.format("INSERT INTO User (username, email, password, create_time) VALUES"
+				+ " ('%s', '%s', '%s', CURRENT_TIMESTAMP)", user.getUsername(), "sd", "ll");
+		DatabaseManager.updateQuery(query);
+	}
+	
+	public static void updateUser(User user) {
+		//String query = String.format("UPDATE User SET username = ", args)
+	}
+	
+	public static void main(String[] args) {
+		UserDatabaseHandler.getAllUsers();
+	}
+	
+}
