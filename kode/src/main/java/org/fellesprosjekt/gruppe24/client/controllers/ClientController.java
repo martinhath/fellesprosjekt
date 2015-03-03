d1c15fb4f60e5b8dbba5bb1cfab665f2a5c871ea
phrida <phridanorrhall@gmail.com>
2015-03-03 13:56:32 +0100

    Merge remote-tracking branch 'origin/client' into client
    Conflicts:
    
    kode/src/main/java/org/fellesprosjekt/gruppe24/client/CalendarApplicatio
    n.java

--- kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/ClientController.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/ClientController.java
@@ -1,12 +1,29 @@
 package org.fellesprosjekt.gruppe24.client.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.Initializable;
+
 import org.fellesprosjekt.gruppe24.client.CalendarApplication;
+import org.fellesprosjekt.gruppe24.client.CalendarClient;
+
+import com.esotericsoftware.kryonet.Client;
 
 public abstract class ClientController implements Initializable{
     private CalendarApplication application;
+    private Client client;
+    
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+        client = CalendarClient.GetInstance().getClient();
+	}
+    
+    public Client getClient(){
+    	return client;
+    }
 
-    public void setApplication(CalendarApplication application){
+	public void setApplication(CalendarApplication application){
         this.application = application;
     }
 
