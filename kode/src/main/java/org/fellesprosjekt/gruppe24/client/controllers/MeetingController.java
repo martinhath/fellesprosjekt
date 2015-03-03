651c422bd0e14f1daeecaef21c181d645109cc09
Martin Thoresen <martinhath@gmail.com>
2015-03-03 11:17:40 +0100

    Lagde div kontrollere

---
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/MeetingController.java
@@ -0,0 +1,11 @@
+package org.fellesprosjekt.gruppe24.client.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MeetingController extends ClientController {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
