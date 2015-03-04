c178544ae375d6263a57540fc80271af86a3ca77
Martin Thoresen <martinhath@gmail.com>
2015-03-04 11:07:16 +0100

    Skrev litt inline docs for serverkontrolleren

--- kode/src/main/java/org/fellesprosjekt/gruppe24/server/controllers/ServerController.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/server/controllers/ServerController.java
@@ -15,12 +15,35 @@ public abstract class ServerController {
         connection = c;
     }
 
+    /**
+     * Brukes når klienten sender info til serveren,
+     * uten at det nødvendigvis skal lagres noe sted.
+     * Eks, innlogging.
+     * @param req Requesten fra klienten.
+     */
     public abstract void post(Request req);
 
+    /**
+     * Brukes når klienten sender info til serveren
+     * som _skal_ lagres. (kanskje vi ikke trenger denne?)
+     * @param req Requesten fra klienten
+     */
     public abstract void put(Request req);
 
+    /**
+     * Brukes når vi skal ha et eller flere objekter
+     * basert på en parameter.
+     * Eks, få brukeren med en Id
+     * Eks, få gruppene en bruker er med i
+     * @param req Requesten fra klienten
+     */
     public abstract void get(Request req);
 
+    /**
+     * Brukes når vi skal få alt av noe.
+     * Eks, få alle brukere
+     * @param req Requetsen fra klienten
+     */
     public abstract void list(Request req);
 
 }
