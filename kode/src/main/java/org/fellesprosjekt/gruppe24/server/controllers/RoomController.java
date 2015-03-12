54d14d5ea2c568241c9200abd0eb996bb064c031
viktorfa <viktorfa@stud.ntnu.no>
2015-03-12 14:59:27 +0100

    Server kan nå hente Room

---
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/server/controllers/RoomController.java
@@ -0,0 +1,53 @@
+package org.fellesprosjekt.gruppe24.server.controllers;
+
+import org.fellesprosjekt.gruppe24.common.models.Meeting;
+import org.fellesprosjekt.gruppe24.common.models.Room;
+import org.fellesprosjekt.gruppe24.common.models.net.Request;
+import org.fellesprosjekt.gruppe24.common.models.net.Response;
+import org.fellesprosjekt.gruppe24.common.models.net.RoomRequest;
+import org.fellesprosjekt.gruppe24.database.MeetingDatabaseHandler;
+import org.fellesprosjekt.gruppe24.database.RoomDatabaseHandler;
+import org.fellesprosjekt.gruppe24.server.ServerConnection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class RoomController extends ServerController {
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    public RoomController(ServerConnection conn) {
+        super(conn);
+    }
+
+    @Override
+    public void post(Request req) {
+        // TODO: implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void put(Request req) {
+        // TODO: implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void get(Request req) {
+        // TODO: implement
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void list(Request req) {
+        Meeting meeting = (Meeting) req.payload;
+        if (meeting == null) Response.GetFailResponse("Meeting was null.");
+        RoomDatabaseHandler rhandler = RoomDatabaseHandler.GetInstance();
+
+        List<Room> rooms = rhandler.getAvailableRooms(meeting);
+
+        Response res = new Response(Response.Type.OK, rooms);
+        connection.sendTCP(res);
+
+    }
+}
---
+++ kode/src/test/java/org/fellesprosjekt/gruppe24/server/ServerRoomTest.java
@@ -0,0 +1,81 @@
+package org.fellesprosjekt.gruppe24.server;
+
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+import junit.framework.TestCase;
+import org.fellesprosjekt.gruppe24.common.KryoUtils;
+import org.fellesprosjekt.gruppe24.common.models.LoginInfo;
+import org.fellesprosjekt.gruppe24.common.models.Meeting;
+import org.fellesprosjekt.gruppe24.common.models.Room;
+import org.fellesprosjekt.gruppe24.common.models.User;
+import org.fellesprosjekt.gruppe24.common.models.net.AuthRequest;
+import org.fellesprosjekt.gruppe24.common.models.net.Request;
+import org.fellesprosjekt.gruppe24.common.models.net.Response;
+import org.fellesprosjekt.gruppe24.common.models.net.RoomRequest;
+import org.fellesprosjekt.gruppe24.database.RoomDatabaseHandler;
+import org.fellesprosjekt.gruppe24.database.UserDatabaseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerRoomTest extends TestCase {
+
+    static CalendarServer server;
+    static Client client;
+    User viktor;
+    User martin;
+    User herman;
+    Meeting meeting1;
+    Meeting meeting2;
+    Room room1;
+    RoomDatabaseHandler rhandler;
+    UserDatabaseHandler uhandler;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        uhandler = UserDatabaseHandler.GetInstance();
+        rhandler = RoomDatabaseHandler.GetInstance();
+        viktor = uhandler.getUserFromUsername("Viktor");
+        room1 = rhandler.get(1);
+
+        int tcp = 6788;
+        int udp = 6789;
+
+        if (server == null) {
+            server = new CalendarServer(tcp, udp);
+            server.start();
+        }
+
+        if (client == null) {
+            client = new Client();
+            client.start();
+            client.connect(5000, "127.0.0.1", tcp, udp);
+
+            KryoUtils.registerClasses(client.getKryo());
+        }
+    }
+
+    public void testGetRoomsForMeeting() throws Exception {
+        Request req = new RoomRequest(Request.Type.LIST, meeting1);
+        client.sendTCP(req);
+
+        client.addListener(new Listener() {
+            @Override
+            public void received(Connection connection, Object object) {
+                super.received(connection, object);
+                try {
+                    Response res = (Response) object;
+                    assertEquals(res.type, Response.Type.OK);
+                    List<Room> rooms = (ArrayList<Room>) res.payload;
+                    assertNotNull(rooms);
+                    assertTrue(rooms.contains(room1));
+                } catch (Exception e) {
+                    fail();
+                }
+                client.removeListener(this);
+            }
+        });
+    }
+}
