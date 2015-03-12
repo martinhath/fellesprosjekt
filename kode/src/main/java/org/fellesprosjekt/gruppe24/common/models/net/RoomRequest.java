948b96ed1e82745b1ba0779a4789ed5210b09ab4
viktorfa <viktorfa@stud.ntnu.no>
2015-03-12 14:59:27 +0100

    Server kan nå hente Room

---
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/common/models/net/RoomRequest.java
@@ -0,0 +1,10 @@
+package org.fellesprosjekt.gruppe24.common.models.net;
+
+public class RoomRequest extends Request {
+
+    public RoomRequest() {}
+
+    public RoomRequest(Type type, Object payload) {
+        super(type, payload);
+    }
+}
