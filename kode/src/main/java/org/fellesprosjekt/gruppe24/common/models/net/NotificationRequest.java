fc40cfcc2f0d6ca2c4bf5b5b636fa94cfdc5fa97
Martin Thoresen <martinhath@gmail.com>
2015-03-12 12:04:08 +0100

    Fikset på notificationrequest, og la inn logikk i authcontroller

--- kode/src/main/java/org/fellesprosjekt/gruppe24/common/models/net/NotificationRequest.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/common/models/net/NotificationRequest.java
@@ -1,4 +1,21 @@
 package org.fellesprosjekt.gruppe24.common.models.net;
 
 public class NotificationRequest extends Request{
+
+    public boolean includeRead;
+
+    public NotificationRequest() {
+        super();
+        includeRead = false;
+    }
+
+    public NotificationRequest(Type t, Object o) {
+        super(t,o);
+        includeRead = false;
+    }
+
+    public NotificationRequest(Type t, boolean includeRead, Object o) {
+        super(t,o);
+        this.includeRead = includeRead;
+    }
 }
