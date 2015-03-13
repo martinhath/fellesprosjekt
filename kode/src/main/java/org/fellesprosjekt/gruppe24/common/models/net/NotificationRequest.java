0561331b573d4fbae77a38e5b5ef54dcadf52b11
viktorfa <viktorfa@stud.ntnu.no>
2015-03-13 14:40:00 +0100

    Lagd controller til Notification med støtte for LIST og PUT for både Group og Meeting

--- kode/src/main/java/org/fellesprosjekt/gruppe24/common/models/net/NotificationRequest.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/common/models/net/NotificationRequest.java
@@ -1,8 +1,13 @@
 package org.fellesprosjekt.gruppe24.common.models.net;
 
+import org.fellesprosjekt.gruppe24.common.models.Entity;
+
 public class NotificationRequest extends Request{
 
     public boolean includeRead;
+    public enum Handler {MEETING, GROUP, BOTH}
+
+    public Handler handler;
 
     public NotificationRequest() {
         super();
@@ -17,5 +22,15 @@ public class NotificationRequest extends Request{
     public NotificationRequest(Type t, boolean includeRead, Object o) {
         super(t,o);
         this.includeRead = includeRead;
+    }
+    public NotificationRequest(Type t, Handler handler , Object o) {
+        super(t,o);
+        this.includeRead = false;
+        this.handler = handler;
+    }
+    public NotificationRequest(Type t, boolean includeRead, Handler handler , Object o) {
+        super(t,o);
+        this.includeRead = includeRead;
+        this.handler = handler;
     }
 }
