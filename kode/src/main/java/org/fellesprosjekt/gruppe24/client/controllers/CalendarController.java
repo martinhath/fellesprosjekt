bbc7533ed45a3bc0c383ae0b902bf79770615b79
Martin Thoresen <martinhath@gmail.com>
2015-03-16 12:38:38 +0100

    vise events i kalenderen del I

--- kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/CalendarController.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/CalendarController.java
@@ -9,15 +9,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import org.fellesprosjekt.gruppe24.client.Layout;
 import org.fellesprosjekt.gruppe24.client.listeners.ClientListener;
+import org.fellesprosjekt.gruppe24.common.models.Meeting;
 import org.fellesprosjekt.gruppe24.common.models.net.AuthRequest;
+import org.fellesprosjekt.gruppe24.common.models.net.MeetingRequest;
 import org.fellesprosjekt.gruppe24.common.models.net.Request;
 import org.fellesprosjekt.gruppe24.common.models.net.Response;
 
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
@@ -37,6 +42,7 @@ public class CalendarController extends ClientController {
     @FXML private Label labelMonth;
     @FXML private Text textBruker;
     
+    @FXML private GridPane calendarGrid;
 
     private DateTimeFormatter dayformat = DateTimeFormatter.ofPattern("EEEE d.");
     private DateTimeFormatter weekformat = DateTimeFormatter.ofPattern("w");
@@ -44,11 +50,67 @@ public class CalendarController extends ClientController {
 
     private LocalDateTime date;
 
+    private List<Meeting> meetings;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         super.initialize(location, resources);
         date = LocalDateTime.now();
         setCalendarLabels(date);
+
+        Request req = new MeetingRequest(Request.Type.LIST, /*getClient().getUser()*/null);
+        getClient().sendTCP(req);
+        getClient().addListener(new ClientListener() {
+            @Override
+            public void receivedResponse(Connection conn, Response res) {
+                if (!(res.payload instanceof List)) {
+                    return;
+                }
+                try {
+                    meetings = (List<Meeting>) res.payload;
+                } catch (ClassCastException e) {
+                    logger.info("Wrong response: " +  res.payload);
+                    return;
+                }
+                showMeetings();
+                getClient().removeListener(this);
+            }
+
+        });
+    }
+
+    /**
+     * Sørger for at alle møter for denne uken vises i kalenderen.
+     */
+    private void showMeetings() {
+        // Fjerner møtene som vises nå.
+        calendarGrid.getChildren().forEach((node) -> {
+            if (node instanceof Pane)
+                calendarGrid.getChildren().remove(node);
+        });
+        if (meetings == null) return;
+        for (Meeting m : meetings) {
+            showMeeting(m);
+        }
+    }
+
+    private void showMeeting(Meeting m) {
+        LocalDateTime from = m.getFrom();
+        LocalDateTime to = m.getTo();
+
+         // Sjekker om møtet begynner i neste uke, eller sluttet i forige uke.
+        LocalDateTime mon = date.minusDays(date.getDayOfWeek().getValue()-1);
+        LocalDateTime sun = date.plusDays(7 - date.getDayOfWeek().getValue());
+        if (from.isAfter(sun)) return;
+        if (to.isBefore(mon)) return;
+
+        int col = from.getDayOfWeek().getValue();
+        int row = from.getHour();
+        System.out.println(col + "\t" + row);
+        // vise drit i kalenderen
+        Pane pane = new Pane();
+        pane.setStyle("-fx-background-color: #ff9933;");
+        calendarGrid.add(pane, col, row);
     }
 
     @Override
@@ -83,12 +145,14 @@ public class CalendarController extends ClientController {
     public void clickPrevWeek(ActionEvent e) {
         date = date.minusWeeks(1);
         setCalendarLabels(date);
+        showMeetings();
     }
 
     @FXML
     public void clickNextWeek(ActionEvent e) {
         date = date.plusWeeks(1);
         setCalendarLabels(date);
+        showMeetings();
     }
 
     @FXML
