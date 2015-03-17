2fd505aeefcb686996c3c0ab810adfaa97cbbbf3
Martin Thoresen <martinhath@gmail.com>
2015-03-17 15:35:28 +0100

    Mer layout. Ikke sikker på hvordan vi bør gjøre dette

--- kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/MeetingDetailController.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/MeetingDetailController.java
@@ -2,16 +2,37 @@ package org.fellesprosjekt.gruppe24.client.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import org.fellesprosjekt.gruppe24.client.Formatters;
 import org.fellesprosjekt.gruppe24.client.Layout;
 import org.fellesprosjekt.gruppe24.common.models.Meeting;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class MeetingDetailController extends ClientController {
 
     private Meeting meeting;
 
-    @FXML
-    public Label labelDesc;
+    @FXML public Label labelTitle;
+    @FXML public TextArea textDesc;
+    @FXML public TextField textRoom;
+    @FXML public DatePicker datePicker;
+    @FXML public TextField textFrom;
+    @FXML public TextField textTo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        super.initialize(location, resources);
+        textDesc.setDisable(true);
+        textRoom.setDisable(true);
+        datePicker.setDisable(true);
+        textFrom.setDisable(true);
+        textTo.setDisable(true);
+    }
 
     public void setMeeting(Meeting m) {
         meeting = m;
@@ -21,11 +42,16 @@ public class MeetingDetailController extends ClientController {
     private void setFields() {
         if (meeting == null) return;
 
-        labelDesc.setText(meeting.getDescription());
+        labelTitle.setText(meeting.getName());
+        textDesc.setText(meeting.getDescription());
+        textRoom.setText(meeting.getRoom().getName());
+        datePicker.setValue(meeting.getFrom().toLocalDate());
+        textFrom.setText(meeting.getFrom().format(Formatters.hhmmformat));
+        textTo.setText(meeting.getTo().format(Formatters.hhmmformat));
     }
 
     public void clickEdit(ActionEvent actionEvent) {
-
+        // hva skal skje her? Åpne nytt vindu, eller gjøre felt redigerbare?
     }
 
     public void clickBack(ActionEvent actionEvent) {
