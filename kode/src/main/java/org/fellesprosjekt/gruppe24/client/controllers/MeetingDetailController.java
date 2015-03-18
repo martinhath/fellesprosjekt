692efaf5458eb2a5d5c381a23f720c7bbc45d381
Martin Thoresen <martinhath@gmail.com>
2015-03-18 01:12:25 +0100

    Endring av meetings skjer i detaljviewet
    Kan bare endres av møtelederen.

--- kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/MeetingDetailController.java
+++ kode/src/main/java/org/fellesprosjekt/gruppe24/client/controllers/MeetingDetailController.java
@@ -15,8 +15,12 @@ public class MeetingDetailController extends ClientController {
 
     private Meeting meeting;
 
+    private boolean isOwner = false;
+    private boolean editMode = false;
+
     @FXML public Label labelTitle;
     @FXML public TextArea textDesc;
+    @FXML public ComboBox<Entity> comboOwner;
     @FXML public TextField textRoom;
     @FXML public DatePicker datePicker;
     @FXML public TextField textFrom;
@@ -25,27 +29,44 @@ public class MeetingDetailController extends ClientController {
     @FXML public ListView<Entity> listParticipants;
     @FXML public ListView<Entity> listInvited;
 
+    @FXML public Button buttonEdit;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         super.initialize(location, resources);
-        textDesc.setDisable(true);
-        textRoom.setDisable(true);
-        datePicker.setDisable(true);
-        textFrom.setDisable(true);
-        textTo.setDisable(true);
+    }
+
+    private void checkAdmin() {
+        if (getApplication().getUser().getId() == meeting.getOwner().getId()){
+            isOwner = true;
+            buttonEdit.setVisible(true);
+        } else {
+            buttonEdit.setVisible(false);
+        }
     }
 
     public void setMeeting(Meeting m) {
         meeting = m;
+        checkAdmin();
         setFields();
     }
 
     private void setFields() {
-        if (meeting == null) return;
+        textDesc.setDisable(!editMode);
+        comboOwner.setDisable(!editMode);
+        textRoom.setDisable(!editMode);
+        datePicker.setDisable(!editMode);
+        textFrom.setDisable(!editMode);
+        textTo.setDisable(!editMode);
 
+        if (meeting == null)
+            return;
+
+        comboOwner.getItems().add(meeting.getOwner());
+        comboOwner.getSelectionModel().select(meeting.getOwner());
         labelTitle.setText(meeting.getName());
         textDesc.setText(meeting.getDescription());
-        textRoom.setText(meeting.getRoom().getName());
+        //textRoom.setText(meeting.getRoom().getName());
         datePicker.setValue(meeting.getFrom().toLocalDate());
         textFrom.setText(meeting.getFrom().format(Formatters.hhmmformat));
         textTo.setText(meeting.getTo().format(Formatters.hhmmformat));
@@ -55,8 +76,25 @@ public class MeetingDetailController extends ClientController {
         }
     }
 
+
+    private boolean validateFields() {
+        // TODO: denne.
+        // bør vise hvilke felter som er gale.
+        return true;
+    }
+
     public void clickEdit(ActionEvent actionEvent) {
-        // hva skal skje her? Åpne nytt vindu, eller gjøre felt redigerbare?
+        if (!validateFields())
+            return;
+
+        if (editMode){
+            editMode = false;
+            buttonEdit.setText("Rediger");
+        } else {
+            editMode = true;
+            buttonEdit.setText("Ferdig");
+        }
+        setFields();
     }
 
     public void clickBack(ActionEvent actionEvent) {
